b44b7e605a25a24fa87e95ee2dc9b7af34cec968
aguttero <aguttero@gmail.com>
2026-03-16 11:43:33 -0300

    0316am02 pymupdf test 01

--- Docling_AI_PDF/docling_convert_v01.py
+++ Docling_AI_PDF/docling_convert_v01.py
@@ -1,6 +1,6 @@
 from docling.document_converter import DocumentConverter
 
-FILE_IN_FOLDER = "docling_ai_pdf/file_in"
+FILE_IN_FOLDER = "./file_in"
 
 #open pdf file normal en python
 # with open (f"{FILE_IN_FOLDER}/Brokerage Statement_2025-12-31_552.PDF", "r") as file:
@@ -9,21 +9,21 @@ FILE_IN_FOLDER = "docling_ai_pdf/file_in"
 # 1. Indicamos la fuente (puede ser una URL o una ruta local)
 # source = "https://arxiv.org"  # Ejemplo de un paper científico en TXT
 
-source = f"{FILE_IN_FOLDER}/Brokerage Statement_2025-12-31_552.PDF"
+# source = f"{FILE_IN_FOLDER}/Brokerage Statement_2025-12-31_552.PDF"
 
-# 2. Inicializamos el convertidor
-converter = DocumentConverter()
+# # 2. Inicializamos el convertidor
+# converter = DocumentConverter()
 
-# 3. Realizamos la conversión
-result = converter.convert(source)
+# # 3. Realizamos la conversión
+# result = converter.convert(source)
 
-# 4. Exportamos a Markdown (ideal para LLMs)
-markdown_output = result.document.export_to_markdown()
+# # 4. Exportamos a Markdown (ideal para LLMs)
+# markdown_output = result.document.export_to_markdown()
 
-with open(f"{FILE_IN_FOLDER}/output_01.md","w", encoding="utf-8") as file:
-    file.write(markdown_output)
+# with open(f"{FILE_IN_FOLDER}/output_01.md","w", encoding="utf-8") as file:
+#     file.write(markdown_output)
 
-print("OK markdown_output 01")
+# print("OK markdown_output 01")
 
 
 
@@ -31,7 +31,7 @@ print("OK markdown_output 01")
 try:
     source_2 = f"{FILE_IN_FOLDER}/response no audit trail_v01.pdf"
     converter = DocumentConverter()
-    result = converter.convert(source)
+    result = converter.convert(source_2)
 except Exception as e:
     print(f"Error converter: {e}")
 
---
+++ pymu_pdf/extract_text_01.py
@@ -0,0 +1,12 @@
+import pymupdf
+
+FOLDER_IN = "./file_in"
+FOLDER_OUT = "./file_out" 
+
+doc = pymupdf.open(f"{FOLDER_IN}/response no audit trail_v01.pdf") # open a document
+out = open(f"{FOLDER_OUT}/output.txt", "wb") # create a text output
+for page in doc: # iterate the document pages
+    text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
+    out.write(text) # write text of page
+    out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
+out.close()
